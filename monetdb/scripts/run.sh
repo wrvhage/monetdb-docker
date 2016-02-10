6460dba3e1990d14db89906f55de1233bd80b176
Willem van Hage <w.vanhage@esciencecenter.nl>
2016-02-10 16:06:37 +0100

    Added docker-compose.yml and rstudio link

--- monetdb/scripts/run.sh
+++ monetdb/scripts/run.sh
@@ -5,7 +5,7 @@ if [ ! -d "/data/dbfarm/db" ]; then
 fi
 
 #/usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
-mserver5 --daemon=no --set mapi_port=50000 --dbinit="include geom; include sql; sql.init(); sql.start();" --dbpath=/data/dbfarm/db --set sql_logdir=/data/dbfarm/db
+mserver5 --daemon=no --set mapi_port=50000 --set mapi_open=true --dbinit="include geom; include sql; sql.init(); sql.start();" --dbpath=/data/dbfarm/db --set sql_logdir=/data/dbfarm/db
 
 
 
